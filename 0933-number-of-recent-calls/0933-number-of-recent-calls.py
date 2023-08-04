9ee66962ce00a65c440590e1c7124af227d71277
sejinkim0101 <109135343+sejinkim0101@users.noreply.github.com>
2023-08-04 14:56:10 +0900

    Time: 223 ms (94.41%), Space: 21.6 MB (43.59%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -5,38 +5,42 @@
 # https://leetcode.com/problems/number-of-recent-calls/discuss/3568692/Beating-99.34-Python-Easiest-Solution
 
 
-# from collections import deque
+from collections import deque
 
-# class RecentCounter:
-#     def __init__(self):
-#         self.requests = deque() # Initialize a double-ended queue to store the requests
+class RecentCounter:
+    def __init__(self):
+        # self.requests = deque() # Initialize a double-ended queue to store the requests
+        self.requests = []
+        
     
-#     def ping(self, t: int) -> int:
-#         self.requests.append(t) # Add the new request time t to the queue
+    def ping(self, t: int) -> int:
+        self.requests.append(t) # Add the new request time t to the queue
         
-#         # Remove requests that are older than 3000 milliseconds from the current time t
-#         while self.requests and self.requests[0] < t - 3000:
-#             self.requests.popleft()
+        # Remove requests that are older than 3000 milliseconds from the current time t
+        while self.requests and self.requests[0] < t - 3000:
+            ## deque를 안 쓰고 pop(0)를 대신 써서도 가능!
+            # self.requests.popleft()
+            self.requests.pop(0)
         
-#         # Return the number of recent requests within the last 3000 milliseconds
-#         return len(self.requests)
+        # Return the number of recent requests within the last 3000 milliseconds
+        return len(self.requests)
 
     
 ## Sol2) GPT4 - without collections 
 
-class RecentCounter:
-    def __init__(self):
-        self.requests = [] # Initialize an empty list to store the requests
+# class RecentCounter:
+#     def __init__(self):
+#         self.requests = [] # Initialize an empty list to store the requests
     
-    def ping(self, t: int) -> int:
-        self.requests.append(t) # Add the new request time t to the list
+#     def ping(self, t: int) -> int:
+#         self.requests.append(t) # Add the new request time t to the list
         
-        # Remove requests that are older than 3000 milliseconds from the current time t
-        # By keeping only those elements in the list that are in the range [t - 3000, t]
-        self.requests = [request for request in self.requests if request >= t - 3000]
+#         # Remove requests that are older than 3000 milliseconds from the current time t
+#         # By keeping only those elements in the list that are in the range [t - 3000, t]
+#         self.requests = [request for request in self.requests if request >= t - 3000]
         
-        # Return the number of recent requests within the last 3000 milliseconds
-        return len(self.requests)
+#         # Return the number of recent requests within the last 3000 milliseconds
+#         return len(self.requests)
 
 
 
