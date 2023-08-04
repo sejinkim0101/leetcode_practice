9b9504dd34cac3e33633d457b55886edb42a2e26
sejinkim0101 <109135343+sejinkim0101@users.noreply.github.com>
2023-08-04 13:08:36 +0900

    Time: 64 ms (99.69%), Space: 15.1 MB (38.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,22 @@
+void moveZeroes(int* nums, int numsSize){
+    int j = 0; // Initialize a pointer j to track the position of the next non-zero element
+
+    // Iterate through the array from the beginning to the end
+    for(int i = 0; i < numsSize; i++) {
+        // Check if the current element is not zero
+        if(nums[i] != 0) {
+            // If it's not zero, place the value at the current j-th position
+            nums[j] = nums[i];
+
+            // If i and j are not pointing to the same position, the moved element was a non-zero element
+            // Replace the original i-th position with zero since it has been moved to j-th position
+            if(i != j) {
+                nums[i] = 0;
+            }
+
+            // Increment j to point to the next position where the next non-zero element should be placed
+            j++;
+        }
+        // If the current element is zero, just continue the loop and move to the next element
+    }
+}
