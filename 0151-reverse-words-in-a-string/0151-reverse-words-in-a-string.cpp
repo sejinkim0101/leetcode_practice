ff4ae6596d9e23f02c2e096d799395ca96d986d6
sejinkim0101 <109135343+sejinkim0101@users.noreply.github.com>
2023-08-25 13:46:27 +0900

    Time: 3 ms (87.24%), Space: 7.4 MB (77.01%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -7,13 +7,16 @@ public:
         // Iterate from the end of the string
         while (i >= 0) {
             while (i >= 0 && s[i] == ' ') i--; // Skip spaces
-            if (i < 0) break; // Check if only spaces were found
+            if (i < 0) break; // Check if only spaces were found // 스페이스만 있던 경우는 아예 밖의 while문을 빠져나옴
             int end = i; // Mark the end of a word
-            while (i >= 0 && s[i] != ' ') i--; // Find the start of the word
+            while (i >= 0 && s[i] != ' ') i--; // Find the start of the Current word
             result += s.substr(i + 1, end - i) + ' '; // Add the word and a space
+                    // e. Extract and Add the Word: The line result += s.substr(i + 1, end - i) + ' '; extracts the word using the substr method and adds it to the result string along with a space. The word is extracted from i + 1 to end, and the space is appended to separate the words in the reversed string.
         }
+        
 
-        // Remove trailing space if present and result is not empty
+
+        // Remove trailing space if present and result is not empty  // trailing은 leading의 반대로 마지막에 있는 스페이스
         if (!result.empty()) {
             result.pop_back();
         }
